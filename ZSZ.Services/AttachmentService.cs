cee9aac27e471627bb24b53c692aeda8338befa0
NextLoad <nextload@163.com>
2019-05-31 19:22:30 +0800

    添加Attachment和cityservice

---
+++ ZSZ.Services/AttachmentService.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZSZ.DTO;
+using ZSZ.IServices;
+using ZSZ.Services.Entities;
+
+namespace ZSZ.Services
+{
+    public class AttachmentService : IAttachment
+    {
+        public long AddAttachment(string name, string iconName)
+        {
+            using (ZSZDbContext ctx = new ZSZDbContext())
+            {
+                AttachmentEntity attachment = new AttachmentEntity();
+                attachment.Name = name;
+                attachment.IconName = iconName;
+                ctx.Attachments.Add(attachment);
+                ctx.SaveChanges();
+                return attachment.Id;
+            }
+        }
+
+        public AttachmentDTO ToDTO(AttachmentEntity attachment)
+        {
+            AttachmentDTO attachmentDto = new AttachmentDTO();
+            attachmentDto.IconName = attachment.IconName;
+            attachmentDto.Name = attachment.Name;
+            attachmentDto.Id = attachment.Id;
+            attachmentDto.CreateDateTime = attachmentDto.CreateDateTime;
+            return attachmentDto;
+        }
+
+        public AttachmentDTO[] GetAll()
+        {
+            using (ZSZDbContext ctx = new ZSZDbContext())
+            {
+                CommonService<AttachmentEntity> cs = new CommonService<AttachmentEntity>(ctx);
+                return cs.GetAll().AsNoTracking().ToList().Select(a => ToDTO(a)).ToArray();
+            }
+        }
+
+        public AttachmentDTO[] GetAttachment(long houseId)
+        {
+            using (ZSZDbContext ctx = new ZSZDbContext())
+            {
+                CommonService<HouseEntity> cs = new CommonService<HouseEntity>(ctx);
+                HouseEntity house = cs.GetById(houseId);
+                if (house == null)
+                {
+                    throw new ArgumentException("房子不存在" + houseId);
+                }
+
+                return house.AttachmentEntities.ToList().Select(a => ToDTO(a)).ToArray();
+            }
+        }
+    }
+}
---
+++ ZSZ.Services/CityService.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZSZ.DTO;
+using ZSZ.IServices;
+using ZSZ.Services.Entities;
+
+namespace ZSZ.Services
+{
+    public class CityService : ICity
+    {
+        public long AddNewCity(string cityName)
+        {
+            CityEntity city = new CityEntity { Name = cityName };
+            using (ZSZDbContext ctx = new ZSZDbContext())
+            {
+                ctx.Cities.Add(city);
+                ctx.SaveChanges();
+                return city.Id;
+            }
+        }
+
+        public CityDTO ToDTO(CityEntity city)
+        {
+            CityDTO cityDto = new CityDTO();
+            cityDto.Name = city.Name;
+            cityDto.Id = city.Id;
+            cityDto.CreateDateTime = city.CreateDateTime;
+            return cityDto;
+        }
+
+        public CityDTO[] GetAll()
+        {
+            using (ZSZDbContext ctx = new ZSZDbContext())
+            {
+                CommonService<CityEntity> cs = new CommonService<CityEntity>(ctx);
+                return cs.GetAll().AsNoTracking().ToList().Select(c => ToDTO(c)).ToArray();
+            }
+        }
+
+        public CityDTO GetById(long id)
+        {
+            using (ZSZDbContext ctx = new ZSZDbContext())
+            {
+                CommonService<CityEntity> cs = new CommonService<CityEntity>(ctx);
+                return ToDTO(cs.GetById(id));
+            }
+        }
+    }
+}
